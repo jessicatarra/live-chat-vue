687a5719a457a2adcaecb72f01d0d5110ab59dca
Jessica Tarra <jessicatarra@Antonios-Mac-mini.local>
2020-12-03 15:59:11 -0300

    adding firebase auth and updating SignupForm component

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+import 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyA5IVXJ8gJ8T6qUXlVBErRBB5Mqe14GBY8",
@@ -14,7 +15,8 @@ const firebaseConfig = {
 // init firebase
 firebase.initializeApp(firebaseConfig)
 
+const projectAuth = firebase.auth()
 const projectFirestore = firebase.firestore()
 const timestamp = firebase.firestore.FieldValue.serverTimestamp
 
-export { projectFirestore, timestamp }
+export { projectAuth, projectFirestore, timestamp }
