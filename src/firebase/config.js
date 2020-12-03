e328526d7462fca3cab250d694ae40e619391ae7
Jessica Tarra <jessicatarra@Antonios-Mac-mini.local>
2020-12-03 14:39:53 -0300

    adding login and signup component and updating welcome.vue to view

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA5IVXJ8gJ8T6qUXlVBErRBB5Mqe14GBY8",
+    authDomain: "live-chat-vue.firebaseapp.com",
+    projectId: "live-chat-vue",
+    storageBucket: "live-chat-vue.appspot.com",
+    messagingSenderId: "780992236287",
+    appId: "1:780992236287:web:303f82d53c5222f3da1aca"
+};
+
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, timestamp }
